7a525d4fb9776896c2b25e293b2f3bf3c545801a
Molkang <mitier26@gmail.com>
2024-11-20 16:02:38 +0900

    Tutorial Basic

---
+++ Tutorial/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private void Start()
+    {
+        //Console.WriteLine("Hello World");
+
+        Debug.Log("Hello Unity");
+    }
+}
