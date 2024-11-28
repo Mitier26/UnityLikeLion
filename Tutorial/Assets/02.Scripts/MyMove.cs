04556ee992e0ad38535050d74f1788570b9fcb51
Molkang <mitier26@gmail.com>
2024-11-28 11:35:33 +0900

    LikeLion - Unity - My Character

---
+++ Tutorial/Assets/02.Scripts/MyMove.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyMove : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    private Animator animator;
+    public float speed = 2;
+    public float jumpPower = 4f;
+    public bool isGround;
+    
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Move();
+       
+        Jump();
+    }
+    
+    private void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        
+        Vector3 dir = new Vector3(h, 0, v).normalized;
+            
+        transform.position += dir * speed * Time.deltaTime;
+
+        if (h == 0 && v == 0)
+        {
+            animator.SetBool("isWalk", false);
+        }
+        else
+        {
+            animator.SetBool("isWalk", true);
+        }
+        
+        if (h != 0 || v != 0)
+        {
+            Vector3 targetPos = transform.position + dir;
+            transform.LookAt(targetPos);
+        }
+    }
+
+    private void Jump()
+    {
+        // 스페이스바를 눌렀을 때
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            rigidbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        }
+    }
+}
