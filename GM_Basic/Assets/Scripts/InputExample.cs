bee100fcf4f8d2e1bf74be9d23a9fadf0312efc3
Molkang <mitier26@gmail.com>
2024-12-04 01:55:58 +0900

    LiKeLion - Unity - GoldMetal Input

---
+++ GM_Basic/Assets/Scripts/InputExample.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputExample : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            Debug.Log("플레이어가 아무 키를 눌렀습니다.");
+        }
+
+        if (Input.anyKey)
+        {
+            Debug.Log("아무키나 누르고 있습니다.");
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("아이템을 구입했습니다");
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("왼쪽으로 이동 중");
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            Debug.Log("오른쪽 이동을 멈추었습니다.");
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("미사일 발사");
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("미사일 모으는 중...");
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("슈퍼 미사일 발사!!"); 
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Input Manager를 이용한 입력!!");
+        }
+        
+    }
+}
