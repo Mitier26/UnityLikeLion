d0e807eae41227fea6fd418fa7789e69c3c10aff
Molkang <mitier26@gmail.com>
2024-12-05 21:58:25 +0900

    LikeLion - Unirty GoldMetal Physics

---
+++ GM_Basic/Assets/Scripts/MyBall.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyBall : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        
+    }
+}
