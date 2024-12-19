549d6695a1e6a69b89cf4a9c4a18d9c360ee3227
Molkang <mitier26@gmail.com>
2024-12-19 17:09:29 +0900

    LikeLion - Unity - Math LineRenderer

--- MathStudy/Assets/Scenes/Cannon.cs
+++ MathStudy/Assets/Scenes/Cannon.cs
@@ -8,17 +8,17 @@ public class Cannon : MonoBehaviour
     public float Mass = 10.0f;
     public int maxStep = 20;
     public float timeStep = 0.1f;
-    
+
     public GameObject CannonBall;
     public GameObject Trajectory;
-    
+    public LineRenderer TrajectoryLine;
+
     public List<GameObject> Objects = new List<GameObject>();
-    
 
     List<Vector3> PredictTrajectory(Vector3 force, float mass)
     {
         List<Vector3> trajectory = new List<Vector3>();
-        
+
         Vector3 position = transform.position;
         Vector3 velocity = force / mass;
 
@@ -27,9 +27,8 @@ public class Cannon : MonoBehaviour
         for (int i = 1; i <= maxStep; i++)
         {
             float timeElapsed = timeStep * i;
-            // 등가속도 운동
-            trajectory.Add(position + 
-                           velocity * timeElapsed + 
+            trajectory.Add(position +
+                           velocity * timeElapsed +
                            Physics.gravity * (0.5f * timeElapsed * timeElapsed));
 
             if (CheckCollision(trajectory[i - 1], trajectory[i], out Vector3 hitPoint))
@@ -41,33 +40,31 @@ public class Cannon : MonoBehaviour
 
         return trajectory;
     }
-    
-    
+
     private bool CheckCollision(Vector3 start, Vector3 end, out Vector3 hitPoint)
     {
         hitPoint = end;
         Vector3 direction = end - start;
         float distance = direction.magnitude;
-        
+
         if (Physics.Raycast(start, direction.normalized, out RaycastHit hit, distance, 1 << LayerMask.NameToLayer("Default")))
         {
             hitPoint = hit.point;
             return true;
         }
-        
+
         return false;
     }
-    
-    // Update is called once per frame
+
     void Update()
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.rotation *= Quaternion.Euler(-90 *Time.deltaTime, 0,0);
+            transform.rotation *= Quaternion.Euler(-90 * Time.deltaTime, 0, 0);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.rotation *= Quaternion.Euler(90*Time.deltaTime,0,0);
+            transform.rotation *= Quaternion.Euler(90 * Time.deltaTime, 0, 0);
         }
 
         if (Input.GetKeyDown(KeyCode.Z))
@@ -82,35 +79,27 @@ public class Cannon : MonoBehaviour
         {
             List<Vector3> trajectorys = PredictTrajectory(transform.forward * Power, Mass);
 
+            // 기존 공 삭제
             foreach (var o in Objects)
             {
                 Destroy(o);
             }
-            
             Objects.Clear();
-            
+
+            // 새로운 궤적 생성
             foreach (var trajectory in trajectorys)
             {
                 var go = Instantiate(Trajectory, trajectory, Quaternion.identity);
                 Objects.Add(go);
             }
-        }
 
-        foreach (var o in Objects)
-        {
-            o.SetActive(false);
+            UpdateLineRenderer(trajectorys);
         }
-        
-        List<Vector3> trajectorys2 = PredictTrajectory(transform.forward * Power, Mass);
+    }
 
-        if (Objects.Count == trajectorys2.Count)
-        {
-            for (var index = 0; index < trajectorys2.Count; index++)
-            {
-                var trajectory = trajectorys2[index];
-                Objects[index].SetActive(true);
-                Objects[index].transform.position = trajectory;
-            }
-        }
+    void UpdateLineRenderer(List<Vector3> points)
+    {
+        TrajectoryLine.positionCount = points.Count;
+        TrajectoryLine.SetPositions(points.ToArray());
     }
 }
