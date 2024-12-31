9b4f84e4d84c614f4ea75a727a734b819f4c6dea
Molkang <mitier26@gmail.com>
2024-12-31 16:16:46 +0900

    LikeLiion - Unity2D - Tetris

--- Tetris/Assets/DesignPatten/Singleton.cs
+++ Tetris/Assets/DesignPatten/Singleton.cs
@@ -8,49 +8,49 @@ using UnityEngine;
 // MonoBehaviour를 가진 것만 싱글톤을 사용할 수 있다.
 public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
-    // static 변수는 단 하나!!
-    private static T instance;
+// static 변수는 단 하나!!
+private static T instance;
 
-    public static T Instance
-    {
-        get
-        {
-            if (instance == null)
-            {
-                instance = FindObjectOfType(typeof(T)) as T; 
-            }
-
-            if (instance == null)
-            {
-                GameObject obj = new GameObject();
-                obj.name = typeof(T).Name;
-                obj.AddComponent<T>();
-            }
-            
-            return instance;
-        }
-    }
-
-    // 실행이 되면 자동으로 실행되게 하는 것이다.
-    // 게임이 시작되면 처음으로 불리우는 함수
-    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-    public static void realSington()
-    {
-        
-    }
-
-    private void Awake()
+public static T Instance
+{
+    get
     {
         if (instance == null)
         {
-            instance = this as T;
+            instance = FindObjectOfType(typeof(T)) as T; 
+        }
+
+        if (instance == null)
+        {
+            GameObject obj = new GameObject();
+            obj.name = typeof(T).Name;
+            obj.AddComponent<T>();
         }
         
-        OnAwake();
+        return instance;
     }
+}
 
-    public virtual void OnAwake()
+// 실행이 되면 자동으로 실행되게 하는 것이다.
+// 게임이 시작되면 처음으로 불리우는 함수
+[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+public static void realSington()
+{
+    
+}
+
+private void Awake()
+{
+    if (instance == null)
     {
-        
+        instance = this as T;
     }
+    
+    OnAwake();
+}
+
+public virtual void OnAwake()
+{
+    
+}
 }
--- Tetris/Assets/Scripts/TetrominoData.cs
+++ Tetris/Assets/Scripts/TetrominoData.cs
@@ -8,9 +8,18 @@ public class TetrominoData : MonoBehaviour
     private List<Transform> blocks = new List<Transform>();
     public List<Transform> Blocks { get => blocks; }
     
-    // Start is called before the first frame update
+    
     void Start()
     {
-        blocks = GetComponentsInChildren<Transform>().ToList();
+        // 주의
+        // transform.GetComponentsInChildren<Transform>()
+        // 이것으로 할 경우 [자신] 포함한다. <--- 에러의 원인
+        for (int i = 0; i < transform.childCount; ++i)
+        {
+            // 자식 오브젝트에 Block 이라는 스크립트를 붙인다.
+            // 테트리스의 한 줄 확인용
+            transform.GetChild(i).gameObject.AddComponent<Block>();
+            blocks.Add(transform.GetChild(i));  
+        }
     }
 }
