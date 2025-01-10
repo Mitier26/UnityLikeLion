04ce2ea00788d5c396f993f09d4df0ac63ecdcbd
Molkang <mitier26@gmail.com>
2025-01-10 10:32:52 +0900

    LikeLion - Unity - 프록시 패턴

---
+++ FSMPattern-3DRPG/Assets/[02]Scripts/UI/IFindPathProxy.cs
@@ -0,0 +1,9 @@
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public interface IFindPathProxy
+{
+    UniTask<Vector3[]> CalculatePath(Vector3 start, Vector3 end);
+    bool IsCalculated { get; }
+    float Progress { get; }
+}
---
+++ FSMPattern-3DRPG/Assets/[02]Scripts/UI/YPathProxy.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
+
+public class YPathProxy : FindMonobehavior, IFindPathProxy
+{
+    public async UniTask<Vector3[]> CalculatePath(Vector3 start, Vector3 end)
+    {
+        _isCalculated = true;
+        _progress = 0.0F;
+
+        try
+        {
+            List<Vector3> path = new List<Vector3>();
+            for (int y = 0; y < 200; ++y)
+            {
+                path.Add(new Vector3(0, y, 0));
+                await UniTask.Delay(200);
+                _progress += y / (float)200;
+            }
+
+            _isCalculated = false;
+
+            return path.ToArray();
+        }
+        finally
+        {
+            _isCalculated = false;
+            _progress = 0.0f;
+        }
+    }
+
+    public bool IsCalculated => _isCalculated;
+    public float Progress => _progress;
+}
