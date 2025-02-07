00f817560cd1fcd1fde61589537ebec230af4677
Molkang <mitier26@gmail.com>
2025-02-07 18:00:17 +0900

    LikeLion - Unity - 틱택토 - Minimax 알고리즘

--- TicTakToe/Assets/Scripts/Game/GameManager.cs
+++ TicTakToe/Assets/Scripts/Game/GameManager.cs
@@ -148,7 +148,8 @@ public class GameManager : Singleton<GameManager>
                 _gameUIController.SetGameUIMode(GameUIController.GameUIMode.TurnB);
                 
                 // TODO : 계산 된 row, col 값
-                var result = AIController.FindNextMove(_board);
+                // var result = AIController.FindNextMove(_board);
+                var result = MinimaxAIController.GetBestMove(_board);
                 if (result.HasValue)
                 {
                     if (SetNewBoardValue(PlayerType.PlayerB, result.Value.row, result.Value.col))
@@ -181,29 +182,12 @@ public class GameManager : Singleton<GameManager>
     {
         if (CheckGameWin(PlayerType.PlayerA)) { return GameResult.Win; }
         if (CheckGameWin(PlayerType.PlayerB)) { return GameResult.Lose; }
-        if (IsAllBlocksPlaced()) { return GameResult.Draw; }
+        if (MinimaxAIController.IsAllBlocksPlaced(_board)) { return GameResult.Draw; }
 
         return GameResult.None;
     }
     
-    /// <summary>
-    /// 모든 마커가 보드에 배치되었는지 확인 하는 함수
-    /// </summary>
-    /// <returns></returns>
-    private bool IsAllBlocksPlaced()
-    {
-        for (var row = 0; row < _board.GetLength(0); row++)
-        {
-            for (var col = 0; col < _board.GetLength(1); col++)
-            {
-                if (_board[row, col] == PlayerType.None)
-                {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+
 
     private bool CheckGameWin(PlayerType playerType)
     {
